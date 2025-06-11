1ed2e76b479771b7f66af57096eed77e6313dda6
ketan-sutar <sutarketan35@gmail.com>
2025-06-11 19:43:33 +0530

    home page structure done

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -8,7 +8,15 @@ const Home = () => {
         className="h-screen flex items-center justify-center bg-white"
       >
         <div>
-          <p>home sec</p>
+          <h1>Hi! I’m [Your Name] 👋 </h1>
+          <h1>Full-stack web developer specialized in MERN & Next.js</h1>
+        </div>
+        <div>
+          <p>
+            I’m a full-stack JavaScript developer with expertise in React.js,
+            Next.js, Node.js, and MongoDB. With hands-on experience building
+            scalable web apps.
+          </p>
         </div>
       </section>
     </div>
